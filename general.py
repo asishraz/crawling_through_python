7dd1b40387e1c832162dbcc0b59b8f477b647a88
asish <iamasish3001@gmail.com>
2020-01-10 00:43:45 +0530

    learned how to create a directory in python

--- general.py
+++ general.py
@@ -1,12 +1,11 @@
 import os
 
-
 def create_directory(directory):
 	if not os.path.exists(directory):
-		print("creating directory " + directory)
-		os.mkdir(directory)
+		print("creating directory : " + directory)
+		os.makedirs(directory)
 	else:
-		print("directory %s already exists" %directory)
+		print("%s already exists" %directory)
 
 
 create_directory('alphaman')
