11e0369d56c80cd8b87583134e7ff0451ca76709
Asish <iamasish3001@gmail.com>
2020-01-16 16:17:07 +0530

    added few lines

--- general.py
+++ general.py
@@ -1,11 +1,27 @@
 import os
 
-def create_directory(directory):
+def create_dir(directory):
 	if not os.path.exists(directory):
-		print("creating directory : " + directory)
+		print("creating directory: " +directory)
 		os.makedirs(directory)
 	else:
-		print("%s already exists" %directory)
+		print("directory already exists")
 
 
-create_directory('alphaman')
+create_dir(input("Enter the directory name: "))
+
+
+def create_data_files(project, base_url):
+	queue = project + 'queue.txt'
+	crawled = project + 'crawled.txt'
+	if not os.path.isfile(queue):
+		write_file(queue, base_url)
+	if not os.path.isfile(crawled):
+		write_file(crawled, '')
+
+#create a new file
+def write_file(path, data):
+	f = open(path, 'w')
+	f.write(data)
+	f.close()
+
