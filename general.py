6cb6e3c4f502092e2bb2519cc4674d9efbca752d
Asish <iamasish3001@gmail.com>
2020-01-16 17:00:18 +0530

    added more lines

--- general.py
+++ general.py
@@ -4,24 +4,29 @@ def create_dir(directory):
 	if not os.path.exists(directory):
 		print("creating directory: " +directory)
 		os.makedirs(directory)
+
 	else:
-		print("directory already exists")
+		print("Directory already exists")
+
+create_dir('w3schools')
 
 
-create_dir(input("Enter the directory name: "))
+#creating data files
+def create_data_files(project_name, base_url):
+	queue = project_name + '/queue.txt'
+	crawled = project_name + '/crawled.txt'
 
-
-def create_data_files(project, base_url):
-	queue = project + 'queue.txt'
-	crawled = project + 'crawled.txt'
 	if not os.path.isfile(queue):
 		write_file(queue, base_url)
 	if not os.path.isfile(crawled):
-		write_file(crawled, '')
+		write_file(crawled, ' ')
 
-#create a new file
 def write_file(path, data):
 	f = open(path, 'w')
 	f.write(data)
 	f.close()
+
+create_data_files('w3schools', 'https://www.w3schools.com/python/default.asp')
+
+
 
